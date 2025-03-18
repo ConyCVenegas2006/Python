d4fe6852fb44d29eaf3bd3a44dc66ae0cc499ae4
ConyCVenegas2006 <constanza.concha.venegas@alumnos.sip.cl>
2025-03-18 08:26:28 -0300

    primer comming

---
+++ carta.py
@@ -0,0 +1,10 @@
+#Constanza Concha
+print("                                                             10/03/2025")
+print("Cony,")
+print("Ahora mismo me siento cansada y con sueño,")
+print("Quiero ser buena en todo lo relacionado a progrmación "
+"porque quiero tener una buena practica.")
+print("Espero que mas adelante seas mas segura para tomar decisiones "
+"y ser una mejor persona tanto en esto como en lo personal.")
+
+print("                                                                    Espero que lo logres!☺️")
---
+++ hola.py
@@ -0,0 +1,2 @@
+#Constanza Concha 10/03/2025
+print("hola mundo")
