150c9d9e67cec1f001d203eccfc697065146b676
ConyCVenegas2006 <constanza.concha.venegas@alumnos.sip.cl>
2025-03-31 12:39:03 -0300

    actividades 2

---
+++ control-de-flujo/nota.py
@@ -0,0 +1,15 @@
+#Constanza Concha
+#31/03/2025
+
+nota = 40
+
+if nota > 70:
+    print("paso")
+elif nota > 60:
+    print("paso")
+elif nota > 50:
+    print("paso")
+elif nota > 40:
+    print("paso")
+else:
+    print("no paso")
