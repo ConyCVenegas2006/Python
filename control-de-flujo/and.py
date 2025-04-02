622337e6d6442209f2d5dd02dd63f7e7f79fe7c6
ConyCVenegas2006 <constanza.concha.venegas@alumnos.sip.cl>
2025-04-02 11:47:21 -0300

    andornot

---
+++ control-de-flujo/and.py
@@ -0,0 +1,10 @@
+#Constanza Concha
+#Tengo sueño
+#02/04/2025
+
+usuario = input ("ingresar tu nombre")
+hambre = int (input ("Cuámta hambre tienes?(1-10)"))
+enojo = int (input ("Cuánto enojo tienes? (1-10)"))
+
+if hambre > 4 and enojo >1:
+    print("Enfadado por hambre")
---
+++ control-de-flujo/or.py
@@ -0,0 +1,12 @@
+#Constanza Concha
+#Tengo sueño
+#02/04/2025
+
+print("Bienvenido a tu día feliz")
+
+cafe = int(input("Cuántas tazas de cafe haz tomado hoy? (0 o más)"))
+te_burbujas = int (input ("Cuántas tazas de te de burbujas has tomado? (0 o más)"))
+if cafe > 0 or te_burbujas > 0:
+    print("¡Que bien! Has disfrutado de algo rico hoy")
+else:
+    print("Parece que no has tomado nada especial hoy. ¡Animate!")
