6fb31208374cdaa0bc133a040c786cd8c5c086fb
ConyCVenegas2006 <constanza.concha.venegas@alumnos.sip.cl>
2025-04-07 08:54:25 -0400

    pincajero

---
+++ Loops/pin.py
@@ -0,0 +1,15 @@
+#Constanza Concha
+#Tengo sueño
+#07/04/2025
+
+print("BANCO DEL 4°B")
+
+pin = int(input ("Ingresa tu PIN:"))
+
+while pin !=1234:
+    print = int (input ("PIN incorrecto. Ingresa nuevamente"))
+
+if pin ==1234:
+    print ("PIN aceptado")
+    print ("Bienvenido a tu cuenta")
+
